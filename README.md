0a43068713550016fed9ab923db3d5eba5a2884a
TONCS010 <150241842+TONCS010@users.noreply.github.com>
2024-01-10 19:13:07 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# VueJs_3_ฺBookShop
+
+Composition API
+"# ComSci_Gadget_Shop" 
---
+++ src/views/FrontEnd/Product.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+    <!-- วนลูปผ่านรายการหมวดหมู่ -->
+    <div v-for="category in categories" :key="category">
+      <h3 class="text-center">{{ category }}</h3>
+      <ProductList :category="category" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProductList from "@/components/FrontEnd/ProductList.vue";
+
+const categories = ["หูฟัง ", "หูฟัง Eadbud", "หูฟัง Full Size", "หูฟัง Gaming", "ลำโพงพกพา"];
+
+
+</script>
+
+<style>
+</style>
